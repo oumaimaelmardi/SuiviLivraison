b15c721e9c582f778b7a9f401e82416649738516
Oumaima EL MARDI <82413301+oumaimaelmardi@users.noreply.github.com>
2023-12-30 14:21:52 +0100

    pull

--- src/main/java/com/example/demo/controllers/ColisController.java
+++ src/main/java/com/example/demo/controllers/ColisController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import com.example.demo.entity.Location;
 import com.example.demo.services.ColisService;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("api/colis")
 public class ColisController {
 	@Autowired
